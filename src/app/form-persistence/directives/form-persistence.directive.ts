82cd9a3b27f7c8a86974c1e86125f0751eb688d8
Basu Dev Adhikari <adbasudev54@gmail.com>
2022-04-22 07:59:24 +0545

    Form persistence

--- src/app/form-persistence/directives/form-persistence.directive.ts
+++ src/app/form-persistence/directives/form-persistence.directive.ts
@@ -1,10 +1,41 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
+import { ControlContainer } from '@angular/forms';
+import { fromEvent, take, takeWhile } from 'rxjs';
 
 @Directive({
   selector: '[appFormPersistence]'
 })
 export class FormPersistenceDirective {
 
-  constructor() { }
+  @Input() name!: string;
+  constructor(private control: ControlContainer) { }
 
+  fromWindowUnloadEvent() {
+    return fromEvent(window, 'beforeunload').pipe(
+      takeWhile(() => this.formGroup?.dirty || false),
+      take(1)
+    );
+  }
+
+  @HostListener('submit')
+  onSubmit() {
+    localStorage.removeItem(this.name);
+  }
+
+  get formGroup() {
+    return this.control.control;
+  }
+
+
+  ngAfterViewInit() {
+    if (localStorage.getItem(this.name)) {
+      this.formGroup?.patchValue(JSON.parse(localStorage.getItem(this.name)!));
+    }
+    this.fromWindowUnloadEvent().subscribe(
+      val => {
+        localStorage.setItem(this.name, JSON.stringify(this.control.value));
+      }
+    );
+  }
 }
+
